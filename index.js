56d6e417636e9a2aae300d8e4ff33f4ed7826c70
hiroki <hiroki0516maeda@gmail.com>
2024-02-17 19:23:51 +0900

    ✅ チャプター終了

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // const hashPassword = async (pw) => {
-//     const salt = await bcrypt.genSalt(12);
-//     const hash = await bcrypt.hash(pw, salt);
-//     console.log(salt);
-//     console.log(hash);
-// }
+//   const salt = await bcrypt.genSalt(12);
+//   const hash = await bcrypt.hash(pw, salt);
+//   console.log(salt);
+//   console.log(hash);
+// };
+
 const hashPassword = async (pw) => {
-    const hash = await bcrypt.hash(pw, 12);
-    console.log(hash);
-}
+  const hash = await bcrypt.hash(pw, 12);
 
-const login = async (pw, hashedPw) => {
-    const result = await bcrypt.compare(pw, hashedPw);
-    if (result) {
-        console.log('ログイン成功！！！！');
-    } else {
-        console.log('ログイン失敗！！！');
-    }
-}
+  console.log(hash);
+};
 
-// hashPassword('123456');
-login('123457', '$2b$12$lo.ahyWPPqhLd4RoK560..wLprbxgz0bM4L0iN8SyC37nps/zFXNW');
+const login = async (pw, hashPassword) => {
+  const result = await bcrypt.compare(pw, hashPassword);
+  if (result) {
+    console.log("ログイン成功!!!!!!!!");
+  } else {
+    console.log("ログイン失敗、、、、、");
+  }
+};
+
+// hashPassword("123456");
+login("123456", "$2b$12$PenODWHNMRJZ4P8wSGZQluSjOt8nLRrxmX0vi0vy9p/5kL8QGrJu2");
